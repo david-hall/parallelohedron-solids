46ab645e526627ec7dafcdf09f56c2012ce62576
John Hall <78449548+ThynStyx@users.noreply.github.com>
2025-12-15 20:16:19 +0000

    Update parallelohedron-solids-listing.js

--- parallelohedron-solids-listing.js
+++ parallelohedron-solids-listing.js
@@ -286,7 +286,7 @@ let pId = parseInt(searchParams.get("P")); // upper case
 if(Number.isNaN(pId)) {
 	pId = parseInt(searchParams.get("p")); // lower case
 }
-if(pId >= 1 && pId <= 43) {
+if(pId >= 1 && pId <= 46) {
 	initialId = pId;
 }
 const initialRow = tbody.rows[ initialId - 1 ];
@@ -363,4 +363,5 @@ function setScene( psolidSceneData ) {
   viewer.update({ camera });
 
 }
+
 
