1f391318db654d01b0a6079d46af59d96154a016
david-hall <David.42.Hall@gmail.com>
2026-01-14 21:54:39 -0500

    Add IKO and JKO and update all others to use corrected strut directions
    Removed Id from the list of models so they will auto-number.
    Added tabindex to the table for keyboard navigation.

--- parallelohedron-solids-listing.js
+++ parallelohedron-solids-listing.js
@@ -11,7 +11,7 @@ const showEdges = document.getElementById( "showEdges" );
 // After the first design is initially rendered,  
 // we don't want to update the camera position with each scene change 
 viewer .addEventListener( "vzome-design-rendered", (e) => {
-  console.log({camera})
+  // console.log({camera})
   camera = false; 
 }, 
 {once: true}); // automatically remove this listener after it is fired once 
@@ -19,14 +19,24 @@ viewer .addEventListener( "vzome-design-rendered", (e) => {
 viewer .addEventListener( "vzome-scenes-discovered", (e) => {
   // Just logging this to the console for now. Not actually using the scenes list.
   const scenes = e.detail;
-  //console.log( "These scenes were discovered in " + viewer.src);
+  // console.log( "These scenes were discovered in " + viewer.src);
   console.log( JSON.stringify( scenes, null, 2 ) );
 } );
 
-for (const psolid of models) {
-  const tr = tbody.insertRow();
-  fillRow(tr, psolid);
-  tr.addEventListener("click", () => selectParallelohedronSolid( psolid, tr ) );
+{ // limit the scope for the id variable
+  let id = 1
+  for (const psolid of models) {
+    const tr = tbody.insertRow();
+    tr.setAttribute('tabindex','0') // Don't increment the tabindex. Leave them all at 0.
+    fillRow(tr, psolid, id++);
+    tr.addEventListener("click", () => selectParallelohedronSolid( psolid, tr ) );
+    tr.addEventListener("keydown", (event) => {
+      if (event.keyCode === 13) { // enter key
+        selectParallelohedronSolid( psolid, tr );
+        event.preventDefault();
+      }
+    } )
+  }
 }
 
 var initialId = 1;
@@ -34,44 +44,41 @@ let pId = parseInt(searchParams.get("P")); // upper case
 if(Number.isNaN(pId)) {
 	pId = parseInt(searchParams.get("p")); // lower case
 }
-if(pId >= 1 && pId <= 90) {
+if(pId >= 1 && pId <= models.length) {
 	initialId = pId;
 }
 const initialRow = tbody.rows[ initialId - 1 ];
 selectParallelohedronSolid( models[ initialId - 1 ], initialRow );
 initialRow.scrollIntoView({ behavior: "smooth", block: "center" });
 
-showEdges.addEventListener("change", // use "change" here, not "click"
-  () => {
-    setScene();
-  } );
+showEdges.addEventListener("change", () => setScene() ) // use "change" here, not "click"
 
 function selectParallelohedronSolid( psolid, tr ) {
 	if(tr != selectedRow) {
-	  const { url, id } = psolid;
+    const { title, url } = psolid;
 		if(url) {
 		  if ( selectedRow ) {
 			  selectedRow.className = "";
 		  }
 		  selectedRow = tr;
 		  selectedRow.className = "selected";
-		  document.getElementById( "index" ).textContent = "P" +id;
+		  document.getElementById( "index" ).textContent = title;
 		  switchModel(url);
 	  } else {
-		  alert("Parallelohedron-solid" + id + " is not yet available.\n\nPlease help us collect the full set.");
+		  alert(`Parallelohedron ${title} is not yet available.\n\nPlease help us collect the full set.`);
 	  }
 	}
 }
 
-function fillRow(tr, psolid) {
-  const { id, title} = psolid;
+function fillRow(tr, psolid, id) {
+  const {title} = psolid;
   if(!tr.id) {
     tr.id = "psolid-" + id;
   }
   // Id column
   let td = tr.insertCell();
   td.className = "ident done";
-  td.innerHTML = "P" + id;
+  td.innerHTML = `P${id}`
   // title column
   td = tr.insertCell();
   td.className = "title";
@@ -120,14 +127,14 @@ function addStrutColumns(tr, title) {
 
 function switchModel(url) {
   let scene = viewer.scene;
-  console.log({url, scene})
+  // console.log({url, scene})
   viewer.src = url;
   setScene();
 }
 
 function setScene() {
   const scene = showEdges.checked ? "Edges" : "Faces";
-  console.log({scene, camera})
+  // console.log({scene, camera})
   viewer.scene = scene;
   viewer.update({ camera });
 }
